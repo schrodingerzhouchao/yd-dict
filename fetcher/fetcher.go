81678a9191260c4b1c337444867b3e2c40261374
schrodingerzhouchao <dadaozc@163.com>
2019-03-22 18:45:53 +0800

    youdao v.1

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -8,36 +8,6 @@ import (
 )
 
 // Fetch fetch the url and return the text of url body.
-/*
-func Fetch(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		//return nil, fmt.Errorf("wrong status code: %d", resp.StatusCode)
-		if resp.StatusCode == http.StatusForbidden {
-			client := &http.Client{}
-			req, err := http.NewRequest("GET", url, nil)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linu…) Gecko/20100101 Firefox/65.0")
-			resp, err = client.Do(req)
-			if err != nil {
-				log.Fatalln(err)
-			}
-		} else {
-			return nil, fmt.Errorf("wrong status code: %d", resp.StatusCode)
-
-		}
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	return body, nil
-}
-*/
 func Fetch(url []byte) ([]byte, error) {
 	resp, err := http.Get(string(url))
 	if err != nil {
